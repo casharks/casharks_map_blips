1df2930c208dcb5b85821bc6b3511c994c9a087c
casharks <145409011+casharks@users.noreply.github.com>
2025-07-09 00:52:26 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+shared_script "@ReaperV4/bypass.lua"
+
+lua54 "yes" -- needed for Reaper
+
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Its already in the clint - blimps lua but go to https://docs.fivem.net/docs/game-references/blips/ to find the shit u need for blimps'
+author 'Demon'
+
+client_scripts {
+    'client/blimps.lua'
+}
